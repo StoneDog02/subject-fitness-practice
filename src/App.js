68cbc8c326bd0c78dd071a4dd449712cdd1165ca
StoneDog02 <87345253+StoneDog02@users.noreply.github.com>
2022-08-17 06:23:05 -0600

    card practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
+import * as React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from "@mui/material";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Card sx={{ maxWidth: 500 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Workout Placeholder
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            This is where the workout of the day will go with whatever
+            description is wanted from the admin and the picture above is added
+            by admin as well
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
